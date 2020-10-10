368026f78f768c2387f16d304ba29fdd76fa256f
Kirill Zhurin <kirillzhurin@gmail.com>
2020-10-11 00:18:07 +0300

    Create rating component

---
+++ frontend/src/components/Rating.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Rating = ({ value, text, color }) => {
+  const stars = [];
+  const maxValue = 5
+
+  for (let index = 1; index <= maxValue; index++) {
+    stars.push(
+      <span>
+        <i style={{ color }}
+          className={
+            value >= index
+              ? 'fas fa-star'
+              : value >= index - .5
+                ? 'fas fa-star-half-alt'
+                : 'far fa-star'
+          }
+        ></i>
+      </span>
+    )
+  }
+
+  return (
+    <div className="rating">
+      {stars}
+
+      <span>{text && text}</span>
+    </div>
+  )
+}
+
+Rating.defaultProps = {
+  color: '#f8e825',
+}
+
+Rating.propTypes = {
+  value: PropTypes.number.isRequired,
+  text: PropTypes.string.isRequired,
+  color: PropTypes.string,
+}
+
+export default Rating
