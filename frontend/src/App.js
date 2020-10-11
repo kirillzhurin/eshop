6ca07993de0dff0d7c5a975b2cb6792c7cab6418
Kirill Zhurin <kirillzhurin@gmail.com>
2020-10-11 20:36:46 +0300

    Include react router dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,23 @@
 import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import HomeScreen from './screens/HomeScreen'
+import Home from './pages/Home'
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
 
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Route path='/' component={Home} exact />
         </Container>
       </main>
 
       <Footer />
-    </>
+    </Router>
   );
 }
 
